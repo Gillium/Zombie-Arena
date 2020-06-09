b97c06cbe81955f55ab43c6dc16ad215f9e76955
Peter Piper <petermpiper@gmail.com>
2020-06-09 06:33:55 -0400

    Created code to use the Zombie class to create a horde
    Created code to use the Zombie class to create a horde

--- Zombie Arena/ZombieArena.h
+++ Zombie Arena/ZombieArena.h
@@ -1,5 +1,8 @@
 #pragma once
-#include <SFML/Graphics.hpp> //ADDED...WAS NOT INSTRUCTED IN BOOK
+//#include <SFML/Graphics.hpp> //ADDED...WAS NOT INSTRUCTED IN BOOK
+#include "Zombie.h"
 
 using namespace sf;
+
 int createBackground(VertexArray& rVA, IntRect arena);
+Zombie* createHorde(int numZombies, IntRect arena);
